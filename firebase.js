a0276fd0f8af212d4999b5b2d9c1a2237524c4b6
Marvin Thai <marvinthai@yahoo.com>
2020-03-15 18:09:00 -0500

    Update firebase.js
    Rotating key

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyASlxya9gi5hZayhEOZdAHddxNzhLe1oiY",
+  apiKey: "AIzaSyCmAoY3w4CXqrWutcYv9r5Mm7mP4KymryY",
   authDomain: "sunny-6120a.firebaseapp.com",
   databaseURL: "https://sunny-6120a.firebaseio.com",
   storageBucket: "sunny-6120a.appspot.com",
